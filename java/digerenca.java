c0c2134d2a0d09ccadea36e55e746946a9ea396c
kevenwallace <kevenwallace_@hotmail.com>
2022-05-31 01:18:56 -0300

    25-lanche

---
+++ java/digerenca.java
@@ -0,0 +1,32 @@
+
+
+/* 
+Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+Entrada
+
+O arquivo de entrada contém 4 valores inteiros.
+Saída
+
+Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade.
+
+*/
+
+import java.util.Scanner;
+
+public class digerenca {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int A = input.nextInt();
+        int B = input.nextInt();
+        int C = input.nextInt();
+        int D = input.nextInt();
+        input.close();
+
+        int diff = (A * B) - (C * D);
+
+        System.out.println("DIFERENCA = " + diff);
+        System.exit(0);
+
+    }
+}
---
+++ python/iniciante/25-lanche.py
@@ -0,0 +1,26 @@
+"""
+Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A seguir, calcule e mostre o valor da conta a pagar.
+
+Entrada
+
+O arquivo de entrada contém dois valores inteiros correspondentes ao código e à quantidade de um item conforme tabela acima.
+Saída
+
+O arquivo de saída deve conter a mensagem "Total: R$ " seguido pelo valor a ser pago, com 2 casas após o ponto decimal.
+"""
+
+""" item 1 = cachorro quente = R$ 4.00
+    item 2 = X-salada        = R$ 4.50
+    item 3 = X-Bacon         = R$ 5.00
+    item 4 = Torrada simples = R$ 2.00
+    item 5 = refrigerante    = R$ 1.50"""
+
+valores = {1: 4.00, 2: 4.50, 3: 5.00, 4: 2.00, 5: 1.50}
+
+pedido = input()
+pedido = pedido.split(None)
+item = int(pedido[0])
+quant = int(pedido[1])
+
+preco = valores[item] * quant
+print(f"Total: R$ {preco:.2f}")
